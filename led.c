b43021cfb25ba28108b17df54864c1f2f294015a
Yi Chen <happyyi008@gmail.com>
2017-05-01 16:55:09 -0400

    render tabs

--- led.c
+++ led.c
@@ -44,7 +44,9 @@ enum editorKey {
 
 typedef struct erow {
     int size;
+    int rsize;
     char *chars;
+    char *render;
 } erow;
 
 struct editorConfig {
@@ -152,10 +154,10 @@ void editorDrawRows(struct abuf* ab) {
             abAppend(ab, "~", 1);
             if (E.numrows == 0 && y == E.screenrows / 3) drawWelcome(ab);
         } else {
-            int len = E.row[filerow].size - E.coloff;
+            int len = E.row[filerow].rsize - E.coloff;
              if (len < 0) len = 0;
             if (len > E.screencols) len = E.screencols;
-            abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+            abAppend(ab, &E.row[filerow].render[E.coloff], len);
         }
 
         abAppend(ab, CLR_LINE);
@@ -324,6 +326,30 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
+void editorUpdateRow(erow* row) {
+    int tabs = 0;
+    int j;
+
+    for (j = 0; j < row->size; j++) {
+        if (row->chars[j] == '\t') tabs++;
+    }
+
+    free(row->render);
+    row->render = malloc(row->size + tabs*3 + 1);
+
+    int idx = 0;
+    for (j = 0; j < row->size; j++) {
+        if (row->chars[j] == '\t') {
+            row->render[idx++] = ' ';
+            while (idx%4 != 0) row->render[idx++] = ' ';
+        } else {
+            row->render[idx++] = row->chars[j];            
+        }
+    }
+    row->render[idx] = '\0';
+    row->rsize = idx;
+}
+
 void editorAppendRow(char *s, size_t len) {
     E.row = realloc(E.row, sizeof(erow)*(E.numrows + 1));
 
@@ -332,6 +358,11 @@ void editorAppendRow(char *s, size_t len) {
     E.row[at].chars = malloc(len + 1);
     memcpy(E.row[at].chars, s, len);
     E.row[at].chars[len] = '\0';
+
+    E.row[at].rsize = 0;
+    E.row[at].render = NULL;
+    editorUpdateRow(&E.row[at]);
+
     E.numrows++;
 }
 
